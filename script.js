ed99b5f1e52b814caca1c53c0b9b3717a45f8d5a
1301315780-svg <1301315780@s.tusd1.org>
2026-03-05 21:06:19 +0000

    done

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ console.log("Script started");
 //Variable to track the number of clicks
 let clicks = 0;
 
+// click value
+let clickValue = 1;
+
 
 // Variable to store the click dispkay element
 let clickDisplay = document.getElementById("click-display");
+
+// Variable for level display
+let levelDisplay = document.getElementById("level-display")
 
 //set the initial of click display
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -13,7 +19,7 @@ clickDisplay.innerText = "Clicks: " + clicks;
 function handleClick() {
     console.log("click");
 
-    clicks = clicks + 1;
+    clicks = clicks + clickValue;
 
     //update display with new value
     clickDisplay.innerText = "Clicks: " + clicks;
@@ -22,5 +28,7 @@ function handleClick() {
 
         alert("level 2");
     }
+    levelDisplay.innerText = "Level 2";
+    clickValue = 2;
 
 }
